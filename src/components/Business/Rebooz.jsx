4b0b4bd7aeed1748009eaaaf9e75dbdec9d88326
Jihee Byun <jh910805jh@gmail.com>
2024-05-07 20:58:57 +0900

    [FEAT] made Rebooze's page structure

--- src/components/Business/Rebooz.jsx
+++ src/components/Business/Rebooz.jsx
@@ -4,11 +4,32 @@ import React from "react";
 import styled from "styled-components";
 
 
+const ReboozeBusinessWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1{
+    font-size: 35px;
+    margin-bottom: 10px;
+    padding: 30px 0 30px 0;
+  }
+  @media (max-width: 450px) {
+    padding: 0 0px 0 0px;
+    margin-bottom: 30px;
+  }
+  span{
+    margin-bottom: 60px;
+  }
+`;
 
 export const ReboozBusiness = () => {
-    return (
-        <>
-            <h2>REREREBOOOZ</h2>
-        </>
-    )
+  return (
+    <>
+      <ReboozeBusinessWrapper>
+        <h1>리부즈 소개</h1>
+      </ReboozeBusinessWrapper>
+    </>
+  )
 }
