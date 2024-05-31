6caf482dd19e91b95fb80b5613d9ddbe34222f24
Jihee Byun <jh910805jh@gmail.com>
2024-05-31 21:08:04 +0900

    [FIX] added CharLe video

--- src/components/Business/CharLeBusiness.jsx
+++ src/components/Business/CharLeBusiness.jsx
@@ -1,9 +1,10 @@
 
-import React from "react";
+import React, { useRef, useState } from "react";
 
 import styled from "styled-components";
 
 import ElectricBoltIcon from '@mui/icons-material/ElectricBolt';
+import ReactPlayer from 'react-player';
 
 const CharLeBusinessWrapper = styled.div`
   width: 100%;
@@ -193,6 +194,7 @@ const CharLeActiveRightContainer = styled.div`
 `;
 
 const ActiveProsConstainer = styled.div`
+  width: 100%;
   display: flex;
   justify-content: center;
   align-items: start;
@@ -212,14 +214,16 @@ const ActiveProsConstainer = styled.div`
   }
   .ActivePropsImg-container{
     width: 100%;
+ 
     height: auto;
     display: flex;
-    justify-content: center;
+    justify-content: space-around;
     align-items: center;
     flex-wrap: wrap;
     flex-direction: row;
     gap: "20px";
     img{
+      max-width: 460px;
       margin: 10px;
       width: 45%;
       height: 500px;
@@ -237,11 +241,68 @@ const ActiveProsConstainer = styled.div`
       }
     }
   }
+`;
+
+
+const CharLeTitleVedioContainer = styled.div`
+  width: 90%;
+  display: flex;
+  overflow: hidden;
+  position: relative;
+  margin-top: 40px;
+  margin-bottom: 100px;
+  .play-btn{
+    transform: translate( -50%, -50% );
+    opacity: .7;
+    width: 80px;
+    height: 80px;
+    right: 0px;
+    bottom: 0px;
+    margin: auto;
+    border-radius: 90px;
+    border-style: solid;
+    border-width: 2px;
+    border-color: #FFFFFF;
+    font-size: 3rem;
+    text-align: center;
+    line-height: 75px;
+  }
+`;
+
+const VideoTextContainer = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  .text-box{
+    display: flex;
+    align-items: center;
+    h2{
+      font-size: 25px;
+      padding: 0 30px 0 30px;
+    }
+  }
 
 `;
 
 
 export const CharLeBusiness = () => {
+  const [ playing, setPlaying ] = useState(false);
+  const videoState = useRef(null);
+  const [ ready, setReady ] = useState(false);
+  const [ curr, setCurr ] = useState(
+    'https://youtu.be/sqgxcCjD04s?si=ePXJiYzUtjTZ7g_e',
+  );
+
+  const onEnded = () => {
+    setCurr('https://youtu.be/ZXmoJu81e6A?si=cqMWOLxy-4PF0dxg');
+    setPlaying(true);
+  };
+
+  const playVideo = () =>{
+    setPlaying((prev)=>!prev)
+  };
+
+
   return (
     <>
       <CharLeBusinessWrapper>
@@ -323,6 +384,33 @@ export const CharLeBusiness = () => {
             <img src={ process.env.PUBLIC_URL + "/img/business/HyundaiActive.png"} alt="pros" style={{marginTop:"20px"}}></img>
           </div>
         </ActiveProsConstainer>
+
+        <VideoTextContainer >
+          <div className="text-box">
+            <ElectricBoltIcon sx={{padding:"0 0px 0 30px", width: "30px", height: "30px"}}></ElectricBoltIcon>
+            <h2>찰리 무선 충전 기술 Active 장점</h2>
+          </div>
+        </VideoTextContainer>
+        <CharLeTitleVedioContainer>
+            <ReactPlayer
+              ref={videoState}
+              url={process.env.PUBLIC_URL + "/videos/CharLebot.mp4"}
+              className='player' // 클래스 이름 지정하여 스타일 적용
+              playing={playing} // 재생 상태, true = 재생중 / false = 일시 정지
+              controls={true} // 유튜브 재생 컨트롤바 노출 여부
+              onEnded={onEnded} // 현재 재생 중인 영상 종료 시 호출
+              onReady={() => setReady(true)} // 영상이 로드되어 준비된 상태
+              width="100%" 
+              height="auto" 
+              muted={true}
+              playIcon={<button>Play</button>}
+              style={{ objectFit: "cover", borderRadius:" 20px", position: "relative" }}
+            >
+            </ReactPlayer>
+
+
+        </CharLeTitleVedioContainer>
+
       </CharLeBusinessWrapper>
 
     </>
